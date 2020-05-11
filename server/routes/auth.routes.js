af50a15b5f206a4e0ccc91508669af35f50fde9a
DayanRojas <dayan18_13@hotmail.com>
2020-05-12 00:18:20 +0200

    auth routes in from working

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -31,4 +31,14 @@ router.delete('/session', ensureLoggedIn(), (req, res, next) => {
     res.status(200).json({ message: 'Log out success!' });
 });
 
+
+router.get('/loggedin', (req, res, next) => {
+    // req.isAuthenticated() is defined by passport
+    if (req.isAuthenticated()) {
+        res.status(200).json(req.user);
+        return;
+    }
+    res.status(403).json({ message: 'Unauthorized' });
+});
+
 module.exports = router
