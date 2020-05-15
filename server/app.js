f68983aaac5d50d3ee142920ca5d1b88bed73508
DayanRojas <dayan18_13@hotmail.com>
2020-05-15 11:56:48 +0200

    deploy

--- server/app.js
+++ server/app.js
@@ -16,11 +16,8 @@ require('./configs/passport.config')(app)
 require('./configs/views.configs')(app)
 require('./configs/locals.config')(app)
 
-
-
-// app.use((req, res) => {
-// res.sendFile(__dirname + "/public/index.html");
-// });
+app.use("/api", require("/routes"))
+app.use((req, res) => {res.sendFile(__dirname + "/public/index.html");})
 
 // Base URLS
 
---
+++ server/public/precache-manifest.ce56d413ec8ea9de03f185d6702a7f8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b713d4755914131e3a71e4149c08d8e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74af5298c904bbbb81e0",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "ba04ea3f5ab0cbfa038b",
+    "url": "/static/css/main.da77a551.chunk.css"
+  },
+  {
+    "revision": "74af5298c904bbbb81e0",
+    "url": "/static/js/2.5a9d46b8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5a9d46b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba04ea3f5ab0cbfa038b",
+    "url": "/static/js/main.1919bde6.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
